70b782ed5ec0ef3dceff527627eea8894132aafc
João Meneses <150847479+jenomi15@users.noreply.github.com>
2024-09-16 01:32:23 -0300

    Delete trabalho-melhorado-main/trabalho-melhorado-main/atual directory

--- trabalho-melhorado-main/trabalho-melhorado-main/atual/src/JogadorBase.java
+++
@@ -1,8 +0,0 @@
-// interface para o decorator dos acessórios//
-public interface JogadorBase {
-    int getMoedasCasaSimples();
-    String getDescricao();
-    void adicionarMoedas(JogadorBase jogador, int moedasCasaSimples);
-    int getPontuacaoEmMoedas();
-    void removerMoeda(int preco);
-}
